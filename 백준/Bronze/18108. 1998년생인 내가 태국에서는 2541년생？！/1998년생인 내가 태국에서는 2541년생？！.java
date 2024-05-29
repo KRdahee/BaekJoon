d8dc47abdeeb157a9fd847bcd2677890fc188278
Dahee <113000888+KRdahee@users.noreply.github.com>
2024-05-29 10:33:56 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner bc = new Scanner(System.in);
+		int year =  bc.nextInt();
+		System.out.println(year - 543);
+	}
+}
